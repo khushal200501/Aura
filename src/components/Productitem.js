3b7bdec3b9ee174f29dd8b68ee3ca042a7d5ea91
Khushal <khushalgupta392@gmail.com>
2025-12-26 12:47:53 +0530

    Logo added

--- src/components/Productitem.js
+++ src/components/Productitem.js
@@ -9,7 +9,7 @@ const ProductItem = ({ product }) => {
       <Link to={`/product/${product.id}`} style={{ textDecoration: 'none', color: 'inherit' }}> 
         <img src={product.image} className='card-img-top' alt='...' style={{ height: '200px', objectFit: 'contain' }}/>
         <div className='card-body'>
-          <h5 className='card-title'>{product.title.slice(0, 30)}...</h5>
+          <h5 className='card-title'>{product.title.slice(0, 15)}...</h5>
         </div>
       </Link>
       <button className='btn btn-primary' onClick={() => addToCart(product)}>
